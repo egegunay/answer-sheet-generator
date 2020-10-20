63c6d0b17514ac1f83a9fce43b2e7243a6de1c9c
EgeGunay <egegunay@protonmail.com>
2020-10-20 20:43:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const requestedValue = parseInt(prompt("How long will your answer sheet will be", "40"))
+const typeOfResult = prompt("Would you like your answer to be an array or a string?", "String").toLowerCase()
+let counter = 1
+let resultArray = []
+let resultString = ""
+
+switch (typeOfResult) {
+    case "string":
+        while (counter <= requestedValue) {
+            resultString += resultString + counter + "-)\n"
+        }
+
+        alert("Check the console!")
+        console.log(resultString)
+        break;
+    
+    case "array":
+        while (counter <= requestedValue) {
+            resultArray.push(counter + "-)")
+            counter++
+        }
+        
+        console.log(resultArray) 
+        alert("Check the console!")
+        break;
+        
+    default:
+        alert("Wha?")
+        break;
+}
