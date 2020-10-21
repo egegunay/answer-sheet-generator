39d7b7747fa612755b8578429d1351727a58c86e
EgeGunay <egegunay@protonmail.com>
2020-10-21 17:21:45 +0300

    WIP

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
 'use strict';
 
-const requestedValue = parseInt(prompt("How long will your answer sheet will be", "40"))
-const typeOfResult = prompt("Would you like your answer to be an array or a string?", "String").toLowerCase()
+
 let counter = 1
-let resultArray = []
 let resultString = ""
 
-switch (typeOfResult) {
-    case "string":
-        while (counter <= requestedValue) {
-            resultString += counter + "-)\n"
-            counter++
-        }
-
-        alert("Check the console!")
-        console.log(resultString)
-        break;
-    
-    case "array":
-        while (counter <= requestedValue) {
-            resultArray.push(counter + "-)")
-            counter++
-        }
-        
-        console.log(resultArray) 
-        alert("Check the console!")
-        break;
-        
-    default:
-        alert("Wha?")
-        break;
+function foo() {
+    let valueRead = parseInt(document.getElementById("input").value)
+    while (counter <= valueRead) {
+        resultString += counter + "-)\n"
+        counter++
+    }
+    document.getElementById("chngme").innerHTML = resultString
+    console.log(resultString)
 }
+
+document.getElementById("btn").addEventListener("click", foo())
